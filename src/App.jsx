5c917091320bf7f3bc474b36ba256512162d0a50
Saim Malik <saim9j8@gmail.com>
2026-01-19 20:29:42 +0500

    fixing bugs

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import Header from "./components/Header";
 import Hero from "./components/Hero";
 import SkillsSection from "./components/SkillsSection";
 import ProjectsSection from "./components/ProjectsSection";
-import TestimonialsSection from "./components/testimonials";
 import ContactSection from "./components/ContactSection";
 import Footer from "./components/Footer";
+import TestimonialsSection from "./components/TestimonialsSection";
 
 const App = () => {
   return (
